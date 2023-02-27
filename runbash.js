33b43c517b86af512c3237aa657bc78786ff5899
Moraxyc <me@morax.icu>
2023-02-27 14:47:41 +0000

    perf(runbash.js): use moment-timezone to parse time
    fix #2

--- runbash.js
+++ runbash.js
@@ -2,9 +2,10 @@ const dotenv = require("dotenv")
 dotenv.config()
 const child_process = require('child_process');
 const tgnotice = require('./tgnotice')
+const moment = require('moment-timezone');
 
 function runbash(filepath, roomid, name, title, fileopentime) {
-  let timeid = `${fileopentime.substring(0, 4)}年${fileopentime.substring(5, 7)}月${fileopentime.substring(8, 10)}日${fileopentime.substring(11, 13)}时${fileopentime.substring(14, 16)}分${fileopentime.substring(17, 19)}秒${fileopentime.substring(20, 27)}`
+  let timeid =moment(fileopentime).tz('Asia/Shanghai').format('YYYY年MM月DD日HH时mm分ss秒SSS')
   let command = `rclone ls "${process.env.rclonedir}/${roomid}-${name}/${timeid}/" 2> /dev/null | wc -l`
   let runbashcommand = `cd ${process.env.workdir} && bash ./runbash.sh "${roomid}-${name}" "${title}" "${timeid}" "${filepath.slice(0, -4)}"`
   child_process.exec(runbashcommand, function (error, stdout) {
